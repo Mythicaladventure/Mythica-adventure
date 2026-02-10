3ae7fb9917eb9d86e5daf6f4339662ec87e92c96
Mythicaladventure <magdield17@gmail.com>
2026-02-10 11:52:46 -0400

    Update GameScene.ts

--- client/scripts/GameScene.ts
+++ client/scripts/GameScene.ts
@@ -1,1 +1,18 @@
+import Phaser from 'phaser';
+import TibiaLoader from './TibiaLoader';
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene');
+    }
+
+    preload() {
+        new TibiaLoader(this).preload();
+    }
+
+    create() {
+        new TibiaLoader(this).create();
+        this.add.text(100, 100, 'Bienvenido a Mythical Adventure', { fontSize: '20px' });
+    }
+}
 
