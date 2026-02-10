d59ef5e91536afa200b80c9fe64f40e44ecf8840
Mythicaladventure <magdield17@gmail.com>
2026-02-10 12:01:17 -0400

    Refactor GameScene to use a single TibiaLoader instance

--- client/scripts/GameScene.ts
+++ client/scripts/GameScene.ts
@@ -2,17 +2,27 @@ import Phaser from 'phaser';
 import TibiaLoader from './TibiaLoader';
 
 export default class GameScene extends Phaser.Scene {
+    loader: TibiaLoader;
+
     constructor() {
         super('GameScene');
+        // Inicializamos el cargador
+        this.loader = new TibiaLoader(this);
     }
 
     preload() {
-        new TibiaLoader(this).preload();
+        // Le pedimos al loader que cargue los archivos
+        this.loader.preload();
     }
 
     create() {
-        new TibiaLoader(this).create();
-        this.add.text(100, 100, 'Bienvenido a Mythical Adventure', { fontSize: '20px' });
+        // Verificamos si cargaron
+        this.loader.create();
+        
+        // Mensaje de bienvenida
+        this.add.text(50, 50, 'Bienvenido a Mythical Adventure', { 
+            fontSize: '20px',
+            fontFamily: 'Arial'
+        });
     }
 }
-
